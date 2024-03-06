bf4368fe0bb179908541e7e674b5223b71804738
Cyrus Neuburger <cyrusneuburger1@gmail.com>
2024-03-06 11:14:29 +0000

    zef

--- models/intermediate/transactions_users_int.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-user_id
-,sum(amount_usd) as total_amount
-,count(transaction_id) as nbr_transaction
-,avg(amount_usd) as avg_transaction
-FROM {{ ref('stg_cleaned__transactions') }}
-WHERE transactions_state LIKE "COMPLETED"
-GROUP BY user_id
---This query makes the total amount transferred and the number of transactions where the transaction was completed per user_id--
--- models/intermediate/users_transactions.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM {{ ref('stg_cleaned__users') }}
-LEFT JOIN {{ ref('transactions_users_int') }}
-USING(user_id)
