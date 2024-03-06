bafcca21185ec043f3cb7e26933bf78b8b496c4b
Charlotte Franck <charlottefranck118@gmail.com>
2024-03-06 11:30:19 +0000

    clean users types and columns names 2

--- models/staging/neobank/stg_neobank__users.sql
+++ models/staging/neobank/stg_neobank__users.sql
@@ -14,10 +14,10 @@ renamed as (
         country,
         city,
         EXTRACT (DATETIME FROM created_date) AS created_date, 
-        CAST(user_settings_crypto_unlocked AS BOOL) AS crypto_unlocked
-        LOWER plan,
+        CAST(user_settings_crypto_unlocked AS BOOL) AS crypto_unlocked,
+        INITCAP (plan) AS plan,
         CAST (attributes_notifications_marketing_push AS STRING) AS notifications_push,
-        CAST (attributes_notifications_marketing_email AS STRING) AS notifications_email
+        CAST (attributes_notifications_marketing_email AS STRING) AS notifications_email,
         num_contacts,
         num_referrals,
         num_successful_referrals
