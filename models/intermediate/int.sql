d581315be13af2891901d4f1581360b5fb9ff0b3
Charlotte Franck <charlottefranck118@gmail.com>
2024-03-06 11:00:26 +0000

    generate stg models + deleted the old ones , modify schema with raw data

--- models/intermediate/int.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM {{ ref('stg_cleaned__notifications') }}
--- models/staging/transactions_users_int.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-users_id
-,sum(amount_usd) as total_amount
-,count(transaction_id) as nbr_transaction
-FROM neobank-416209.neobank.transactions_cleaned
-WHERE transactions_state LIKE "COMPLETED"
-GROUP BY user_id
---This query makes the total amount transferred and the number of transactions where the transaction was completed per user_id
