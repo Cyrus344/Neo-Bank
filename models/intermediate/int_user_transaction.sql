f001c89c9a03b5dfcdc720120c659d77855606eb
Cyrus Neuburger <cyrusneuburger1@gmail.com>
2024-03-07 10:23:52 +0000

    update of the yml file for int to match int_user_transaction and int_user_transaction2

--- models/intermediate/int_user_transaction.sql
+++ models/intermediate/int_user_transaction.sql
@@ -4,5 +4,5 @@ user_id
 ,count(transaction_id) as nbr_transaction
 ,avg(amount_usd) as avg_transaction
 FROM {{ ref('stg_neobank__transactions') }}
-WHERE t_state LIKE "COMPLETED"
+WHERE transactions_state LIKE "COMPLETED"
 GROUP BY user_id
