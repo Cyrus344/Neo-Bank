07c105f1dcd6f424ba3c0ad464c742f37ae6fd70
Cyrus Neuburger <cyrusneuburger1@gmail.com>
2024-03-08 13:51:16 +0000

    ntm

---
+++ models/mart/transaction_cohort.sql
@@ -0,0 +1,26 @@
+WITH month_year AS (
+    SELECT
+        user_id,
+        created_date,
+        EXTRACT(YEAR FROM created_date) AS year,
+        EXTRACT(MONTH FROM created_date) AS month
+    FROM {{ ref('stg_neobank__users') }}
+)
+,cohort_month AS (
+    SELECT
+        user_id,
+        created_date,
+        CONCAT(year,"/",month) AS cohort
+    FROM month_year
+)
+, setup_join as (
+SELECT
+user_id
+,created_date
+,cohort
+FROM cohort_month
+)
+SELECT *
+FROM setup_join 
+RIGHT JOIN {{ ref('stg_neobank__transactions') }}
+USING(user_id)
